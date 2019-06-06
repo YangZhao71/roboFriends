9a4f9ba447510cc8c6cf397d72dd5b0517dfbb08
Yangmengyuan Zhao <zymy701@gmail.com>
2019-06-06 16:53:33 +0800

    project exhibition

---
+++ build/precache-manifest.706def8a096c4fdd05dcb27444a01afd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb6856fdb39b3784ad13dfd5271b2d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2622e6476c0519ef7908",
+    "url": "/static/css/main.29af408b.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "2622e6476c0519ef7908",
+    "url": "/static/js/main.93b1eb29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
