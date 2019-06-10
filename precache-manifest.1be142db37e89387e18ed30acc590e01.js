ef1637e31a91574ba047522f2560b48fcac82c93
Yangmengyuan Zhao <zymy701@gmail.com>
2019-06-10 13:47:01 +0800

    Updates

---
+++ precache-manifest.1be142db37e89387e18ed30acc590e01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ff6ae5dae0b0eb7450166adb8d1588",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/roboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "06e01ae7f3b6a644b964",
+    "url": "/roboFriends/static/css/main.5446106c.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/roboFriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "06e01ae7f3b6a644b964",
+    "url": "/roboFriends/static/js/main.7f2e74a4.chunk.js"
+  },
+  {
+    "revision": "0d6209f39b04744aca3d",
+    "url": "/roboFriends/static/js/runtime~main.0e8c0065.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
